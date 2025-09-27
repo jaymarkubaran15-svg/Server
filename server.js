6286b065df6b3828bf1ce47045866ad818313937
Jay Mark <jaymarkubaran15@gmail.com>
2025-09-27 21:29:51 +0800

    Update server.js

--- server.js
+++ server.js
@@ -35,6 +35,9 @@ db.connect((err) => {
   if (err) console.error("MySQL connection failed:", err);
   else console.log("Connected to MySQL");
 });
+app.get("/", (req, res) => {
+  res.send("✅ Backend is running on Render!");
+});
 
 
 //CHECKING FOR ERROR IN EVENT POSTING
