92bf37bffa091c05998c1c7a8e72213b1cfe36a7
Jay Mark <jaymarkubaran15@gmail.com>
2025-09-28 20:18:53 +0800

    Update server.js

--- server.js
+++ server.js
@@ -655,7 +655,7 @@ function sendVerificationEmail(email, token) {
   const transporter = nodemailer.createTransport({
       host: "smtp-relay.brevo.com",
       auth: {
-          user: "9809b0002@smtp-brevo.com",
+          user: "jaymarkubaran15@gmail.com",
           pass:process.env.SENDINBLUE_SMTP_KEY,
       },
   });
